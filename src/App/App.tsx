751abbe139015ca1e398f99410c2079aa154f714
Iván Guerrero <iguerrero@lendingpoint.com>
2022-11-30 09:17:58 -0500

    Use API to load items

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,3 +1,34 @@
+import { useState, useEffect } from "react";
+
+import { TItem } from "../features/items/types";
+
+import itemsAPI from "../features/items/api";
+import { ELoadingStatus } from "./enums";
+
 export default function App(): JSX.Element {
-  return <div>Hello</div>;
+  const [items, setItems] = useState<TItem[]>([]);
+  const [loadingStatus, setLoadingStatus] = useState<ELoadingStatus>(
+    ELoadingStatus.Idle
+  );
+
+  useEffect(() => {
+    setLoadingStatus(ELoadingStatus.Loading);
+    itemsAPI.list().then((items) => {
+      setItems(items);
+      setLoadingStatus(ELoadingStatus.Idle);
+    });
+  }, []);
+
+  if (loadingStatus === ELoadingStatus.Loading) {
+    return <span>Loading...</span>;
+  }
+
+  return (
+    <main>
+      <header>
+        <h1>Supermarket List</h1>
+        <h3>{items.length} item(s)</h3>
+      </header>
+    </main>
+  );
 }
---
+++ src/App/enums.ts
@@ -0,0 +1,4 @@
+export enum ELoadingStatus {
+  Idle,
+  Loading,
+}

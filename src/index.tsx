8451b35767c4682c8143c0e610d02dc45dbe5a2e
Iván Guerrero <iguerrero@lendingpoint.com>
2022-11-30 07:45:53 -0500

    Setup for TS

--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,13 @@ import ReactDOM from "react-dom/client";
 
 import "./styles/reset.css";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+import App from "./App";
+
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
 root.render(
   <React.StrictMode>
-    <div>Hello</div>
+    <App />
   </React.StrictMode>
 );
